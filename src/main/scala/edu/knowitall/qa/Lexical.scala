348562725e1b3c5ac0c04d77ad9260ed32c0f8be
Anthony Fader <afader@gmail.com>
2013-07-07 18:15:28 -0700

    more skeleton code

--- src/main/scala/edu/knowitall/qa/Lexical.scala
+++ src/main/scala/edu/knowitall/qa/Lexical.scala
@@ -1,6 +1,11 @@
 package edu.knowitall.qa
 
+import edu.knowitall.collection.immutable.Interval
+import PartialFunction._
 
+/* Question Tokens are either strings, or two special tokens used to represent
+ * variables (RelVar or $r, and ArgVar or $e).
+ */
 trait QToken
 case class QWord(word: String) extends QToken {
   override def toString = word
@@ -15,14 +20,31 @@ case object ArgVar extends QToken {
   override def toString = "$e"
 }
 
+/* Argument orderings are used to represent whether an argument binds to the
+ * arg1 slot first, or the arg2 slot. Composing two functions will possibly
+ * result in a different argument ordering.
+ */
 sealed abstract trait ArgOrder
 case object Arg1First extends ArgOrder
 case object Arg2First extends ArgOrder
+object ArgOrder {
+  def compose(o1: ArgOrder, o2: ArgOrder) = (o1, o2) match {
+    case (Arg1First, Arg1First) => Arg1
+    case (Arg1First, Arg2First) => Arg2
+    case (Arg2First, Arg1First) => Arg2
+    case (Arg2First, Arg2First) => Arg1
+  }
+}
 
+/* Constant symbols used to represent Arg1 and Arg2 slots. */
 sealed abstract trait Arg
 case object Arg1 extends Arg
 case object Arg2 extends Arg
 
+/* An abstract "simple" query against a triplestore. Has the form either
+ * (?, relation, entity) or (entity, relation, ?) and represents querying a
+ * triplestore for all arguments that can be plugged in for ?.
+ */
 case class SimpleQuery(relation: String, entity: String, queryField: Arg) {
   override def toString = queryField match {
     case Arg1 => s"(?, $relation, $entity)"
@@ -30,13 +52,20 @@ case class SimpleQuery(relation: String, entity: String, queryField: Arg) {
   }
 }
 
+/* Lexical Items associate question tokens with some semantic interpretation. */
 trait LexItem
 
+/* An entity lexical item associates question words (not including question 
+ * variables $r or $e) with a string entity.
+ */
 case class EntItem(words: IndexedSeq[QWord], entity: String) extends LexItem {
   val ws = words.mkString(" ")
   override def toString = s"$ws = $entity"
 }
 
+/* A relation lexical item associates question words (not including question 
+ * variables $r or $e) with a string relation and an argument ordering.
+ */
 case class RelItem(words: IndexedSeq[QWord], relation: String, 
     argOrder: ArgOrder) extends LexItem {
   val ws = words.mkString(" ")
@@ -46,7 +75,9 @@ case class RelItem(words: IndexedSeq[QWord], relation: String,
   } }
 }
 
-    
+/* A question lexical item associates question words that have exactly one
+ * relation variable $r and one entity variable $e with an argument ordering.
+ */    
 case class QuestionItem(words: IndexedSeq[QToken], argOrder: ArgOrder) 
 	extends LexItem {
    
@@ -62,23 +93,31 @@ case class QuestionItem(words: IndexedSeq[QToken], argOrder: ArgOrder)
   }}
 }
 
-case class QREDerivation(
+/* A span of type T associates some span of tokens with an object of type T
+ * (a lexical item).
+ */
+case class Span[+T](interval: Interval, item: T) {
+  def overlaps(that: Span[_]) = this.interval.intersect(that.interval).size > 0
+}
+
+/* A derivation represents a combination of lexical items into a query. A 
+ * derivation composes a question item, an entity item, and a relation item
+ * to generate the query.
+ */
+case class Derivation(
     question: IndexedSeq[QWord], 
     questionItem: QuestionItem, 
-    relItem: RelItem, 
-    entItem: EntItem) { 
+    relSpan: Span[RelItem], 
+    entSpan: Span[EntItem]) { 
 	  
+  val relItem = relSpan.item
+  val entItem = entSpan.item
   val lexItems = IndexedSeq(questionItem, relItem, entItem)
   
   val qws = question.mkString(" ")
   val qOrder = questionItem.argOrder
   val rOrder = relItem.argOrder
-  val queryField = (qOrder, rOrder) match {
-    case (Arg1First, Arg1First) => Arg1
-    case (Arg1First, Arg2First) => Arg2
-    case (Arg2First, Arg1First) => Arg2
-    case (Arg2First, Arg2First) => Arg1
-  }
+  val queryField = ArgOrder.compose(qOrder, rOrder)
   
   val query = SimpleQuery(relItem.relation, entItem.entity, queryField)
   
@@ -87,7 +126,123 @@ case class QREDerivation(
   
 }
 
+/* A lexicon provides access to a set of lexical items. The lexical items are
+ * indexed by the question tokens.
+ */
 trait Lexicon {
+  
   def get(words: IndexedSeq[QToken]): IndexedSeq[LexItem]
+  def getRel(words: IndexedSeq[QToken]): IndexedSeq[RelItem]
+  def getEnt(words: IndexedSeq[QToken]): IndexedSeq[EntItem]
+  def getQuestion(words: IndexedSeq[QToken]): IndexedSeq[QuestionItem]
+  
   def has(words: IndexedSeq[QToken]): Boolean
+  
 }
+
+/* An implementation of a Lexicon represented as a Scala Map object. */
+case class MapLexicon(items: Map[IndexedSeq[QToken], IndexedSeq[LexItem]]) 
+  extends Lexicon {
+  
+  type QTokens = IndexedSeq[QToken]
+  type LexItems = IndexedSeq[LexItem]
+  
+  def get(words: QTokens) = items.getOrElse(words, IndexedSeq())
+  def has(words: QTokens) = items.contains(words)
+  def getRel(words: QTokens) = get(words) flatMap {
+    case x : RelItem => x :: Nil 
+    case _ => Nil
+  }
+  def getEnt(words: QTokens) = get(words) flatMap {
+    case x : EntItem => x :: Nil
+    case _ => Nil
+  }
+  def getQuestion(words: QTokens) = get(words) flatMap {
+    case x : QuestionItem => x :: Nil
+    case _ => Nil
+  }
+  
+}
+
+/* A parser maps a question to a set of derivations using a lexicon. */ 
+abstract class Parser {
+  
+  val lexicon: Lexicon
+  
+  def intervals(size: Int) =
+    for (i <- Range(0, size); j <- Range(i, size)) yield (i, j+1)  
+
+  
+  def enumerateItemSpans(words: IndexedSeq[QWord]): Iterable[Span[LexItem]] = 
+    for ((i, j) <- intervals(words.size); // for each span index
+         ws = words.slice(i, j);		  // get the subseq of words
+         iv = Interval.open(i, j);		  // make an interval
+         item <- lexicon.get(ws))		  // for each lexical item matching words
+      yield Span(iv, item) 			 	  // yield a new Span
+
+  def parse(words: IndexedSeq[QWord]): Iterable[Derivation]
+
+}
+
+/* An implementation of a Parser. It operates by finding all non-overlapping
+ * spans of entity and relation items in the question. Then, it checks to see
+ * if the resulting question pattern exists in the lexicon.
+ */
+case class BottomUpParser(lexicon: Lexicon) extends Parser {
+  
+  /* Generates a question pattern. For example, suppose we have the input 
+   * question "who likes joe" and the lexicon indicated that 
+   * "likes" goes to some relation lexitem, and "joe" goes to some entity
+   * lexitem. Then this function will return the question pattern "who $r $e".
+   */
+  def questionPatFrom(words: IndexedSeq[QWord], rel: Span[LexItem], 
+      ent: Span[LexItem]): IndexedSeq[QToken] = {
+    val rInt = rel.interval
+    val eInt = ent.interval
+    for (i <- 0 to words.size - 1) yield i match {
+      case i if rInt.min == i => RelVar
+      case i if eInt.min == i => ArgVar
+      case i if !rInt.contains(i) && !eInt.contains(i) => words(i)
+    }
+  }
+  
+  def rSpan(s: Any): Option[Span[RelItem]] = s match {
+    case Span(iv, i: RelItem) => Some(Span(iv, i))
+    case _ => None
+  }
+  
+  def eSpan(s: Any): Option[Span[EntItem]] = s match {
+    case Span(iv, i: EntItem) => Some(Span(iv, i))
+    case _ => None
+  }
+  
+  /* Gets all non-overlapping relation and entity spans in the question.*/
+  def relEntSpans(words: IndexedSeq[QWord]) = {
+    val spans = enumerateItemSpans(words)
+    val eSpans = spans.flatMap(eSpan)
+    val rSpans = spans.flatMap(rSpan)
+    for (r <- rSpans; e <- eSpans; if !r.overlaps(e)) yield (r, e)
+  }
+  
+  def parse(words: IndexedSeq[QWord]) = {
+    for ((r, e) <- relEntSpans(words);
+        qpat = questionPatFrom(words, r, e);
+        qitem <- lexicon.getQuestion(qpat))
+      yield Derivation(words, qitem, r, e)
+  }
+  
+}
+
+object Test extends Application {
+  
+  val q: IndexedSeq[QWord] = IndexedSeq("who", "like", "joe")
+  val subj = QuestionItem(IndexedSeq(QWord("who"), RelVar, ArgVar), Arg2First)
+  val joe = EntItem(IndexedSeq("joe"), "j123")
+  val r1 = RelItem(IndexedSeq("like"), "like_rel", Arg2First)
+  val r2 = RelItem(IndexedSeq("like"), "liked-by_rel", Arg1First)
+  val map = Map(subj.words -> IndexedSeq(subj), r1.words -> IndexedSeq(r1, r2), joe.words -> IndexedSeq(joe))
+  val lex = MapLexicon(map)
+  val parser = BottomUpParser(lex)
+  println(parser.parse(q))
+}
+
