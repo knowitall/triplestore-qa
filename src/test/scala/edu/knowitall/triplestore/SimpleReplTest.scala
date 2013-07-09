8263caa697be412681d8634d75faa41d7449b0f5
Anthony Fader <afader@gmail.com>
2013-07-09 08:38:39 -0700

    updated abstractquery tests

--- src/test/scala/edu/knowitall/triplestore/SimpleReplTest.scala
+++ src/test/scala/edu/knowitall/triplestore/SimpleReplTest.scala
@@ -4,13 +4,20 @@ import org.scalatest.FlatSpec
 
 class SimpleReplTest extends FlatSpec {
   
+  val sq = AbstractQuery.fromString("r", "$x, is, fruit")
   "AbstractQuery" should "parse correctly" in {
-    val sq = AbstractQuery.fromString("$x, is, fruit")
     val s = sq match {
       case Some(q) => q.toString
       case None => ""
     }
     assert("($x, is, fruit)" === s)
+  }
+  it should "identify literals" in {
+    val n = sq match {
+      case Some(q) => q.literalFields.size
+      case _ => 0
+    }
+    assert(2 === n)
   }
 
 }
