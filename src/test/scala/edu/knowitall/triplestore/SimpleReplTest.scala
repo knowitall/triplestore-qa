d3e9c2d87dd751b026a6978a7e48234e7308dee6
Anthony Fader <afader@gmail.com>
2013-07-09 21:15:04 -0700

    code for testing repl-related stuff

--- src/test/scala/edu/knowitall/triplestore/SimpleReplTest.scala
+++ src/test/scala/edu/knowitall/triplestore/SimpleReplTest.scala
@@ -1,5 +1,6 @@
 package edu.knowitall.triplestore
 import org.scalatest.FlatSpec
+import Tabulator.{tuplesToTable => toTable}
 
 
 class SimpleReplTest extends FlatSpec {
@@ -19,5 +20,37 @@ class SimpleReplTest extends FlatSpec {
     }
     assert(2 === n)
   }
+  
+  val input = """($x, eats, $y) ($y, is, fruit)"""
+  val sqs = AbstractQuery.fromStringMult(input).toList.sortBy(_.toString)
+  it should "handle multiple query input" in {
+    assert(2 === sqs.size)
+    assert("($x, eats, $y)" === sqs(0).toString)
+    assert("($y, is, fruit)" === sqs(1).toString)
+  }
+  
+  val varx = Variable("x")
+  val vary = Variable("y")
+  
+  val sq2 = AbstractQuery.fromString("r", "$x, is made from, $y")
+  "QueryNode" should "identify join attrs" in {
+    val node = QueryNode(sq2.get)
+    val attrs = node.joinAttrs
+    val expected = Map(varx -> List("r.arg1"), vary -> List("r.arg2"))
+    assert(expected === attrs)
+  }
+  
+  
+  val sq3 = AbstractQuery.fromString("s", "$y, high in, antioxidants")
+  it should "merge join attrs" in {
+    val node2 = QueryNode(sq2.get)
+    val node3 = QueryNode(sq3.get)
+    val attrs2 = node2.joinAttrs
+    val attrs3 = node3.joinAttrs
+    val merged = Joiner.mergeJoinAttrs(attrs2, attrs3)
+    val expected = Map(varx -> List("r.arg1"), vary -> List("r.arg2", "s.arg1"))
+    assert(expected === merged)
+  }
+  
 
 }
