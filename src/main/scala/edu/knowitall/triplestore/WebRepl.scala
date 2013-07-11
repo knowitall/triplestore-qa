8183fd59a24b64587e0f0bbe236e3dd47acfd407
Anthony Fader <afader@gmail.com>
2013-07-10 17:45:55 -0700

    web server for repl

---
+++ src/main/scala/edu/knowitall/triplestore/WebRepl.scala
@@ -0,0 +1,56 @@
+package edu.knowitall.triplestore
+import org.slf4j.LoggerFactory
+import unfiltered.filter.Intent
+import unfiltered.response.ResponseString
+import unfiltered.response.ContentType
+import unfiltered.response.Ok
+import unfiltered.jetty.Http
+import unfiltered.request.GET
+import unfiltered.request.Path
+import unfiltered.request.Seg
+import unfiltered.response.NotFound
+import unfiltered.request.Mime
+import java.io.File
+import unfiltered.response.ContentEncoding
+
+
+
+object WebRepl extends App {
+
+  val logger = LoggerFactory.getLogger(this.getClass) 
+ 
+  object Plan extends unfiltered.filter.Plan {
+    
+    val repl = SimpleRepl()
+    
+    def intent = Intent {
+      
+      case req @ GET(Path(Seg("query" :: Nil))) => 
+        runQuery(req.parameterValues("q").mkString(" "))
+
+      case req @ GET(Path(path)) => getStatic(path)
+        
+    }
+    
+    def runQuery(query: String) = {
+      val result = repl.eval(query)
+      ResponseString(result) ~> Ok      
+    }
+    
+    def getStatic(path: String) = {
+      val mimeType = Mime.unapply(path).getOrElse("text/plain")
+      val realPath = "web/" + { if (path.trim() == "/") "index.html" else path } 
+      val stream = getClass.getClassLoader().getResourceAsStream(realPath)
+        if (stream != null) {
+          val content = io.Source.fromInputStream(stream).mkString
+          System.out.println(mimeType)
+          ContentEncoding(mimeType) ~> ResponseString(content) ~> Ok
+        } else {
+          ResponseString("Not found!") ~> NotFound
+        }
+      }
+  }
+  
+  Http(8080).filter(Plan).run()
+
+}
