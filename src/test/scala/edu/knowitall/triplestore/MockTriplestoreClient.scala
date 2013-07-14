ba90ba42ed0d2d55eea221057a7a58c8519e55fe
Anthony Fader <afader@gmail.com>
2013-07-14 11:38:54 -0700

    server now loads triples.txt into index

--- src/test/scala/edu/knowitall/triplestore/MockTriplestoreClient.scala
+++ src/test/scala/edu/knowitall/triplestore/MockTriplestoreClient.scala
@@ -7,8 +7,18 @@ import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
 import org.apache.solr.core.CoreContainer
 import java.io.File
 import org.apache.commons.io.IOUtils
+import scala.io.Source
 import java.io._
+import org.apache.solr.common.SolrInputDocument
 
+/**
+ * Creates a temporary triplestore solr server that can be used for testing.
+ * It will look on the classpath for the files necessary to create a solr 
+ * instance (schema.xml, solr.xml, solrconf.xml, etc.). It then looks for the
+ * file triples.txt and adds them to the index. 
+ * 
+ * This object will create temporary files to store the index.
+ */
 object MockSolrServer {
   
   def resource(name: String) = {
@@ -24,6 +34,32 @@ object MockSolrServer {
   def copy(from: String, dir: File) = 
     IOUtils.copy(resource(from), outstream(dir, from))
   
+  def lineToDoc(line: String): Option[SolrInputDocument] = {
+    val fields = line.trim().split("\t")
+    if (fields.size == 3) {
+      val arg1 = fields(0)
+      val rel = fields(1)
+      val arg2 = fields(2)
+      val doc = new SolrInputDocument()
+      doc.addField("arg1", arg1)
+      doc.addField("rel", rel)
+      doc.addField("arg2", arg2)
+      doc.addField("namespace", "test")
+      Some(doc)
+    } else {
+      None
+    }
+  }
+  
+  def addDocs(in: InputStream, server: EmbeddedSolrServer) = {
+    val lines = Source.fromInputStream(in).getLines
+    for ((line, i) <- lines.zipWithIndex;
+         doc <- lineToDoc(line)) {
+      doc.addField("id", i.toString)
+      server.add(doc)
+    }
+    server.commit()
+  }
 
   // Create the temp directory for the server
   val solrHome = File.createTempFile("solr", null)
@@ -46,14 +82,22 @@ object MockSolrServer {
   System.setProperty("solr.solr.home", solrHome.getAbsolutePath())
   val initializer = new CoreContainer.Initializer()
   val coreContainer = initializer.initialize()
+  val server = new EmbeddedSolrServer(coreContainer, "collection1")
+  
+  
+  // Add the documents
+  addDocs(resource("triples.txt"), server)
     
-  def getServer = new EmbeddedSolrServer(coreContainer, "collection1")
+  def getServer = server
 
   def shutdown = coreContainer.shutdown()
   
 }
 
-class MockTriplestoreClient extends TriplestoreClient {
+/**
+ * A mock triplestore client that is backed by the mock Solr server.
+ */
+case class MockTriplestoreClient() extends TriplestoreClient {
 
   val server = MockSolrServer.getServer
   
