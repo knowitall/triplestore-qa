727f9d4d1d3a0572a1cc4e006f471f080bc62361
Rob Bart <rbart@cs.washington.edu>
2013-07-14 15:10:53 -0700

    Move SolrLexiconTest.

--- src/main/scala/edu/knowitall/qa/SolrLexicon.scala
+++ src/main/scala/edu/knowitall/qa/SolrLexicon.scala
@@ -45,3 +45,13 @@ class SolrLexicon(val server: SolrServer) extends WeightedLexicon {
     server.query(buildCountQuery(words)).getResults().getNumFound() > 0
   }
 }
+
+object SolrLexiconTest extends App {
+
+   val solrLexicon = new SolrLexicon()
+   val referenceLexItems = new EvalLexiconLoader(args(0))
+   
+   referenceLexItems foreach { item =>
+     require(solrLexicon.get(item.words).map(_.asInstanceOf[LexItem]).toSet.contains(item), s"Solr doesn't seem to have an entry for: $item")
+   }
+}
