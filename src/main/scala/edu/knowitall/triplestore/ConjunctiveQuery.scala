b4661da5325d21bf5624ee905a7c3ecbbebfa1c0
Anthony Fader <afader@gmail.com>
2013-07-15 16:59:00 -0700

    moved and renamed code for conjunctive queries

---
+++ src/main/scala/edu/knowitall/triplestore/ConjunctiveQuery.scala
@@ -0,0 +1,146 @@
+package edu.knowitall.triplestore
+
+import Search.Field._
+import Search.TSQuery
+import edu.knowitall.triplestore.Search.{FieldKeywords, FieldPhrase}
+import edu.knowitall.triplestore.Search.Conjunction
+import org.slf4j.LoggerFactory
+
+/**
+ * Base trait for triple values.
+ */
+trait TVal
+
+/**
+ * Base trait for triple literal values.
+ */
+trait TLiteral {
+  val value: String
+  def toConjunct(field: Field): TSQuery
+}
+
+/**
+ * Triple literals can be unquoted, which has the semantics of doing a keyword 
+ * search over a field.
+ */
+case class UnquotedTLiteral(value: String) extends TVal with TLiteral {
+  override def toString = value
+  override def toConjunct(field: Field) = FieldKeywords(field, value)
+}
+
+/**
+ * Triple literals can also be quoted, which has the semantics of doing an
+ * exact-match search over a field.
+ */
+case class QuotedTLiteral(value: String) extends TVal with TLiteral {
+  override def toString = s""""$value""""
+  override def toConjunct(field: Field) = FieldPhrase(field, value)
+}
+
+/**
+ * Triple values can also be variables, which have a string name.
+ */
+case class TVariable(name: String) extends TVal {
+  override def toString = "$" + name
+}
+
+/**
+ * TConjunct objects have a name (you can think of this as a unique
+ * identifier for the relational table returned) and map containing the
+ * field values. The field values map a field (arg1, rel, or arg2) to a 
+ * value (literal or variable).
+ */
+case class TConjunct(name: String, values: Map[Field, TVal]) {  
+  
+  def literalFields: Iterable[(Field, TLiteral)] = { 
+    for ((f, v) <- values) yield v match {
+      case l: UnquotedTLiteral => Some((f, l))
+      case l: QuotedTLiteral => Some((f, l))
+      case _ => None
+    }
+  }.flatten
+  
+  def variableFields: Iterable[(Field, TVariable)] = {
+    for ((f, v) <- values) yield v match {
+      case TVariable(s) => Some((f, TVariable(s)))
+      case _ => None
+    }
+  }.flatten
+  
+  def varsToFields: Map[TVariable, Field] = variableFields.map(_.swap).toMap
+  
+  def partialQuery: TSQuery = {
+    val lfs = literalFields.toList
+    val conjuncts = for ((f, v) <- lfs) yield v.toConjunct(f)
+    Conjunction(conjuncts.toList:_*)
+  }
+  
+  def joinKeys: Map[TVariable, String] = {
+    val vfs: Map[TVariable, Field] = varsToFields
+    val pairs = for (v <- vfs.keys; f <- vfs.get(v); a = name + "." + f)
+      yield (v, a)
+    pairs.toMap
+  }
+  
+  def vars: Iterable[TVariable] = varsToFields.keys.toSet
+  
+  val xs = values.getOrElse(arg1, "")
+  val rs = values.getOrElse(rel, "")
+  val ys = values.getOrElse(arg2, "")
+  override def toString = s"($xs, $rs, $ys)"
+}
+case object TConjunct {
+  
+  val logger = LoggerFactory.getLogger(this.getClass) 
+  
+  val qpat = """\(?(.+),(.+),(.+?)\)?""".r
+  val vpat = """\$(.*)""".r
+  def fromString(name: String, s: String): Option[TConjunct] = s match {
+    case qpat(x, r, y) => Some(fromTriple(name, x, r, y))
+    case _ => None
+  }
+  
+  val quoted = """^"(.*)"$""".r
+  def getTVal(s: String): TVal = s match {
+    case vpat(v) => TVariable(v)
+    case "?" => TVariable("?")
+    case quoted(v) => QuotedTLiteral(v) 
+    case _ => UnquotedTLiteral(s)
+  }
+  
+  val fields = List(arg1, rel, arg2)
+  def fromTriple(name: String, x: String, r: String, y: String): TConjunct = {
+    val lst = List(x.trim(), r.trim(), y.trim())
+    val items = for ((f, a) <- fields.zip(lst); v = getTVal(a)) yield (f, v)
+    TConjunct(name, items.toMap)
+  }
+  
+  val splitPat = """(?<=\))\s*?(?=\()"""
+  def fromStringMult(s: String): Iterable[TConjunct] = {
+    val parts = s.split(splitPat).toList.map(_.trim).filterNot(_ == "")
+    val queries = { for ((s, i) <- parts.zipWithIndex; 
+                       q <- fromString(s"r$i", s)) yield q }.toList
+    queries
+  }
+} 
+
+/**
+ * A conjunctive query consists of a qvar (query variable) and a list of 
+ * conjuncts. A conjunctive query represents a select-join-project type
+ * operation. The list of conjuncts represents the data to be selected.
+ * The shared variables among the conjuncts encodes the join predicates. 
+ * The qvar encodes the column to project onto. 
+ */
+case class ConjunctiveQuery(qVar: TVariable, conjuncts: List[TConjunct]) {
+  
+  val conjunctNames = conjuncts.map(_.name)
+  if (conjunctNames.distinct.size != conjunctNames.size) throw new 
+    IllegalArgumentException(s"Conjuncts must have distinct names: $conjuncts")
+  
+  val qas = {for (c <- conjuncts; a <- c.varsToFields.get(qVar)) yield a}.toList
+  val qAttr = qas match {
+    case a :: Nil => a
+    case _ => throw new IllegalArgumentException(s"Query variable $qVar must "
+        + "appear in exactly one conjunct in $conjuncts")
+  }
+}
