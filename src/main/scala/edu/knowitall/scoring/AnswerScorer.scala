987d2052d9edfc445d0dfe271a23d49761f7c492
Anthony Fader <afader@gmail.com>
2013-07-15 21:59:37 -0700

    skeleton for answer scoring code

--- src/main/scala/edu/knowitall/scoring/AnswerScorer.scala
+++ src/main/scala/edu/knowitall/scoring/AnswerScorer.scala
@@ -1,5 +1,21 @@
 package edu.knowitall.scoring
 
-trait AnswerScorer {
+import edu.knowitall.execution.AnswerDerivation
 
+trait AnswerScorer {
+  def scoreAnswer(answer: String, derivs: List[AnswerDerivation]): ScoredAnswer
+}
+
+trait ScoredAnswer {
+  def answer: String
+  def derivs: List[AnswerDerivation]
+  def score: Double
+}
+
+case class BasicScoredAnswer(answer: String, derivs: List[AnswerDerivation], 
+    score: Double) extends ScoredAnswer
+    
+case class UniformAnswerScorer(s: Double = 0.0) extends AnswerScorer {
+  override def scoreAnswer(answer: String, derivs: List[AnswerDerivation]) = 
+    BasicScoredAnswer(answer, derivs, s)
 }
