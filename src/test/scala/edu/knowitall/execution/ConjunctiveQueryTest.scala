6297627477c687bb43cae8fd1afcb86c19261442
Anthony Fader <afader@gmail.com>
2013-07-16 13:10:11 -0700

    fixed bug in parsing conj queries from string

--- src/test/scala/edu/knowitall/execution/ConjunctiveQueryTest.scala
+++ src/test/scala/edu/knowitall/execution/ConjunctiveQueryTest.scala
@@ -4,15 +4,24 @@ import org.scalatest.FlatSpec
 
 class ConjunctiveQueryTest extends FlatSpec {
   
-  val q = "$x : ($x, contains, potassium)"
-  
   "ListConjunctiveQuery" should "parse strings correctly" in {
+    val q = "$x : ($x, contains, potassium)"
     val result = ListConjunctiveQuery.fromString(q) match {
       case Some(cq: ListConjunctiveQuery) => cq
       case _ => throw new IllegalArgumentException("")
     }
     assert(TVariable("x") === result.qVar)
     assert(1 === result.conjuncts.size)
+  }
+  
+  it should "handle joins" in {
+    val q = """$x : (salad, with, $x) ($x, source of, protein) ($x, compatible with, vegetarians)"""
+    val result = ListConjunctiveQuery.fromString(q) match {
+      case Some(cq: ListConjunctiveQuery) => cq
+      case _ => throw new IllegalArgumentException("")
+    }
+    assert(TVariable("x") === result.qVar)
+    assert(3 === result.conjuncts.size)
   }
 
 }
