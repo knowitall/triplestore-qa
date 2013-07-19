cd8ca176d3f334224d4a350ea22b6457c4614c7c
Anthony Fader <afader@gmail.com>
2013-07-19 16:43:39 -0700

    moved code for cartesian product into utils

--- src/main/scala/edu/knowitall/execution/ConjunctiveQuery.scala
+++ src/main/scala/edu/knowitall/execution/ConjunctiveQuery.scala
@@ -236,13 +236,10 @@ case object ListConjunctiveQuery {
     }
   }
 
-  def combine[A](xs: Traversable[Traversable[A]]): Seq[Seq[A]] =
-    xs.foldLeft(Seq(Seq.empty[A])) {
-    (x, y) => for (a <- x.view; b <- y) yield a :+ b
-  }
+
   def expandSetTLiterals(cq: ConjunctiveQuery): List[ConjunctiveQuery] = {
     val css = for (c <- cq.conjuncts; cs = TConjunct.expandSetTLiterals(c)) yield cs
-    val product = combine[TConjunct](css).toList
+    val product = Utils.cartesian[TConjunct](css).toList
     for (cs <- product) yield ListConjunctiveQuery(cq.qVar, cs.toList)
   }
 
@@ -271,6 +268,9 @@ case object SimpleQuery {
   }
 }
 
-trait TConjunctRewriteRule {
-  def rewrite(c: TConjunct): List[TConjunct]
+object Utils {
+  def cartesian[A](xs: Traversable[Traversable[A]]): Seq[Seq[A]] =
+    xs.foldLeft(Seq(Seq.empty[A])) {
+    (x, y) => for (a <- x.view; b <- y) yield a :+ b
+  }
 }
