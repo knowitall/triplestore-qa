b3c17297c1f164cddcc0fcaf3ed7ace5c2f94d1f
Rob Bart <rbart@cs.washington.edu>
2013-07-26 15:25:35 -0700

    Provide an iterable-like interface rather than printing to an outputstream.

--- src/main/scala/edu/knowitall/scoring/training/AnswerAnnotator.scala
+++ src/main/scala/edu/knowitall/scoring/training/AnswerAnnotator.scala
@@ -11,28 +11,12 @@ import edu.knowitall.execution.ExecConjunctiveQuery
  * Output is labeled by a human and then fed into TrainingDataReader, which
  * provides an Iterable of labeled answergroups for a training function.
  */
-class AnswerAnnotator(val input: Iterator[String], val output: PrintStream) {
+class AnswerAnnotator(val input: Iterator[String]) {
 
-  case class InputRecord(nlQuery: String, uQuery: String)
-  object InputRecord {
-    def fromString(str: String) = str.split("\t") match {
-      case Array(nlq, uq, _*) => InputRecord(nlq, uq)
-      case _ => throw new RuntimeException(s"Unrecognized input record: $str")
-    }
-  }
-
-  case class OutputRecord(answer: String, uQuery: String, nlQuery: String) {
-    override def toString = Seq(answer, uQuery, nlQuery).mkString("\t")
-  }
-  object OutputRecord {
-    def fromGroup(input: InputRecord, group: AnswerGroup): OutputRecord = {
-      OutputRecord(group.alternates.head.head, input.uQuery, input.nlQuery)
-    }
-  }
-  
   import edu.knowitall.parsing.FormalQuestionParser
   import edu.knowitall.execution.{IdentityExecutor, BasicAnswerGrouper}
   import edu.knowitall.triplestore.{SolrClient, CachedTriplestoreClient}
+  import AnswerAnnotator.{InputRecord, OutputRecord}
   
   val parser = new FormalQuestionParser()
   val baseClient = SolrClient("http://rv-n12.cs.washington.edu:8983/solr/triplestore", 500)
@@ -47,36 +31,62 @@ class AnswerAnnotator(val input: Iterator[String], val output: PrintStream) {
     val uQueries = parser.parse(input.uQuery)
     val answers = uQueries flatMap executor.deriveAnswers
     val groups = grouper group answers.toList
-    val sortedGroups = groups.sortBy(-_.derivations.size).take(10) 
+    val sortedGroups = groups.sortBy(-_.derivations.size).take(25) 
     sortedGroups map { g => OutputRecord.fromGroup(input, g) }
   }
   
-  def go(): Unit = {
+  def outputRecords: Iterator[OutputRecord] = {
     
-    val answers = inputRecords flatMap answersFor
-    
-    answers foreach output.println
+    inputRecords flatMap answersFor
   }
 }
 
 object AnswerAnnotator extends App {
-  
+
   import scopt.OptionParser
   import java.io.File
   import edu.knowitall.common.Resource.using
-  
-  case class Config(
-      val inputQueries: File = new File("."),
-      val output: PrintStream = System.out)
-  
-  val parser = new OptionParser[Config]("AnswerAnnotator") {
-    arg[File]("inputQueries") action { (file, config) => config.copy(inputQueries = file) } text("Tab delimited file with columns: Natural Language Query | Formal Query")
-    opt[File]("outFile") action { (file, config) => config.copy(output = new PrintStream(file)) } text("Optional output file, default stdout.")
+
+  case class InputRecord(nlQuery: String, uQuery: String)
+  object InputRecord {
+    val splitRegex = "\t".r
+    def fromString(str: String) = splitRegex.split(str) match {
+      case Array(nlq, uq, _*) => InputRecord(nlq, uq)
+      case _ => throw new RuntimeException(s"Unrecognized input record: $str")
+    }
   }
-  
+
+  case class OutputRecord(answer: String, uQuery: String, nlQuery: String) {
+    override def toString = Seq(answer, uQuery, nlQuery).mkString("\t")
+  }
+  object OutputRecord {
+    import InputRecord.splitRegex
+    def fromGroup(input: InputRecord, group: AnswerGroup): OutputRecord = {
+      OutputRecord(group.alternates.head.head, input.uQuery, input.nlQuery)
+    }
+    def fromString(s: String) = {
+      val splits = splitRegex.split(s).map(_.trim)
+      splits match {
+        case Array(answer, uQuery, nlQuery) => OutputRecord(answer, uQuery, nlQuery)
+        case a => throw new RuntimeException(s"Invalid OutputRecord format: ${a.mkString(" ")}")
+      }
+    }
+  }
+
+  case class Config(
+    val inputQueries: File = new File("."),
+    val output: PrintStream = System.out)
+
+  val parser = new OptionParser[Config]("AnswerAnnotator") {
+    arg[File]("inputQueries") action { (file, config) => config.copy(inputQueries = file) } text ("Tab delimited file with columns: Natural Language Query | Formal Query")
+    opt[File]("outFile") action { (file, config) => config.copy(output = new PrintStream(file)) } text ("Optional output file, default stdout.")
+  }
+
   parser.parse(args, Config()).foreach { config =>
     using(io.Source.fromFile(config.inputQueries)) { source =>
-      new AnswerAnnotator(source.getLines, config.output).go()
+      val annotator = new AnswerAnnotator(source.getLines)
+      val outputRecords = annotator.outputRecords
+      outputRecords foreach config.output.println
     }
     if (config.output != System.out) config.output.close()
   }
