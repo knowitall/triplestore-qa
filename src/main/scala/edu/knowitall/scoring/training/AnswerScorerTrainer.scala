12efd762234d18247a19b35f4e35d90c0ddee9f6
Rob Bart <rbart@cs.washington.edu>
2013-07-25 17:19:06 -0700

    Add helper method and tool for saving model to disk.

--- src/main/scala/edu/knowitall/scoring/training/AnswerScorerTrainer.scala
+++ src/main/scala/edu/knowitall/scoring/training/AnswerScorerTrainer.scala
@@ -1,27 +1,36 @@
 package edu.knowitall.scoring.training
 
+import edu.knowitall.tool.conf.impl.LogisticRegression
 import edu.knowitall.tool.conf.BreezeLogisticRegressionTrainer
 import edu.knowitall.execution.AnswerGroup
 import edu.knowitall.scoring.AnswerGroupFeatures
+import edu.knowitall.tool.conf.Labelled
+import java.io.FileOutputStream
 
 object AnswerScorerTrainer {
   
-  def trainingResource = {
-    val url = getClass.getResource("scorer-training.txt")
-    require(url != null, "Could not find resource: scorer-training.txt")
-    url
-  }
-  
   def trainer = new BreezeLogisticRegressionTrainer[AnswerGroup](AnswerGroupFeatures.featureSet)
   
-  def trainingData = new TrainingDataReader(trainingResource)
   
-  def classifier = {
-    val classifier = trainer.train(trainingData)
+  def trainClassifier(training: Iterable[Labelled[AnswerGroup]]) = {
+    val classifier = trainer.train(training)
     val weights = classifier.featureWeights.iterator
     weights.foreach { case (feature, weight) =>
       System.err.println(s"$feature\t$weight")  
     }
     classifier
+  }
+  
+  lazy val loadDefaultClassifier = trainClassifier(TrainingDataReader.defaultTraining)
+  
+  def main(args: Array[String]): Unit = {
+    
+    val savePath = args(0)
+    
+    val outStream = new FileOutputStream(savePath)
+    
+    loadDefaultClassifier.save(outStream)
+    
+    outStream.close()
   }
 }
