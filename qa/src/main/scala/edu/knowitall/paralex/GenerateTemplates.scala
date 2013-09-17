1817d80d96e7f2e92b27da8c137e9e454600947e
Anthony Fader <afader@gmail.com>
2013-09-17 09:39:40 -0700

    init code for paralex

---
+++ qa/src/main/scala/edu/knowitall/paralex/GenerateTemplates.scala
@@ -0,0 +1,38 @@
+package edu.knowitall.paralex
+
+import scala.io._
+import com.nicta.scoobi.Scoobi._
+import com.nicta.scoobi.Scoobi.ScoobiApp
+import com.nicta.scoobi.io.text.TextInput
+import com.nicta.scoobi.io.text.TextSource
+import com.nicta.scoobi.core.Reduction
+import com.nicta.scoobi.core.DList
+
+object GenerateTemplates extends ScoobiApp {
+  
+  val generator = TemplateGenerator()
+  
+  def lineToTemplates(line: String): List[String] = {
+    val c = QACluster.fromString(line)
+    generator.generateTemplates(c).map(_.toString)
+  }
+  
+  def run() {
+    val lines = TextInput.fromTextSource(new TextSource(Seq(args(0))))
+    val clusters = lines.mapFlatten(lineToTemplates)
+    persist(clusters.toTextFile(args(1), true))    
+  }
+  
+}
+
+object GenerateTemplatesCLI extends App {
+  val generator = TemplateGenerator()
+  def lineToTemplates(line: String): List[String] = {
+    val c = QACluster.fromString(line)
+    generator.generateTemplates(c).map(_.toString)
+  }
+  val groups = Source.fromInputStream(System.in, "UTF8").getLines.grouped(100)
+  groups.foreach { lines => 
+  	lines.par.flatMap(lineToTemplates) foreach println
+  }
+}
---
+++ qa/src/main/scala/edu/knowitall/paralex/Paralex.scala
@@ -0,0 +1,106 @@
+package edu.knowitall.paralex
+
+import edu.knowitall.parsing.regex.RegexQuestionParser
+import edu.knowitall.execution.IdentityExecutor
+import edu.knowitall.triplestore.SolrClient
+import edu.knowitall.execution.BasicAnswerGrouper
+import edu.knowitall.scoring.UniformAnswerScorer
+import edu.knowitall.apps.QASystem
+import org.slf4j.LoggerFactory
+import edu.knowitall.tool.stem.MorphaStemmer
+import edu.knowitall.tool.tokenize.ClearTokenizer
+import edu.knowitall.execution.SimpleQuery
+import edu.knowitall.execution.UQuery
+import edu.knowitall.execution.ListConjunctiveQuery
+import edu.knowitall.parsing.QWord
+import edu.knowitall.parsing.QuestionParser
+import edu.knowitall.tool.tokenize.Tokenizer
+import edu.knowitall.parsing.QToken
+import edu.knowitall.execution.Search
+import edu.knowitall.parsing.ArgVar
+import edu.knowitall.execution.TConjunct
+import edu.knowitall.execution.TVariable
+
+case class QACluster(answers: Seq[String], questions: Seq[String])
+
+case object QACluster {
+  val prefix = "^([qa]:)(.*)".r
+  def trim(s: String) = s match {
+    case prefix(l, r) => r
+    case _ => s
+  }
+  def fromString(s: String): QACluster = {
+    val parts = s.split("\t").toSeq
+    val questions = parts.filter(p => p.startsWith("q:")).map(trim).distinct
+    val answers = parts.filter(p => p.startsWith("a:")).map(trim).distinct
+    QACluster(answers, questions)
+  }
+}
+
+case class OutputRecord(template: Seq[QToken], query: TConjunct) {
+  override def toString = template.mkString(" ") + "\t" + query.toString
+}
+
+case class ParsedQuestion(string: String, qWords: List[QWord], parses: List[TConjunct])
+
+case class AbstractedArg(arg: List[QWord], conjunct: TConjunct)
+
+case class TemplateGenerator(parser: QuestionParser = RegexQuestionParser(),
+    tokenizer: Tokenizer = new ClearTokenizer()) {
+  
+  def filterQuery(uq: UQuery): Option[TConjunct] = uq match {
+    case ListConjunctiveQuery(List(v), List(c)) => Some(TConjunct(c.name, c.values))
+    case _ => None
+  }
+  
+  def getArg(sq: TConjunct): AbstractedArg = {
+    val fields = sq.literalFields
+    val fields2 = for ((f, v) <- fields; if f == Search.arg1 || f == Search.arg2) 
+      yield (f,v)
+    val argField = fields2 match {
+      case List((Search.arg1, v)) => Search.arg1
+      case List((Search.arg2, v)) => Search.arg2
+      case _ => throw new IllegalStateException(s"Could not find non-variable arg in $fields")
+    }
+    val arg = toQWords(sq.values(argField).toString().toLowerCase())
+    val newConj = TConjunct(sq.name, sq.values + (argField -> TVariable("y")))
+    AbstractedArg(arg, newConj)
+  }
+  
+  def toQWords(s: String) = tokenizer.tokenize(s).map(t => QWord(t.string)).toList
+  
+  def parseQuestion(q: String): ParsedQuestion = {
+    val parses = parser.parse(q).flatMap(filterQuery).toList
+    val qWords = toQWords(q.toLowerCase())
+    ParsedQuestion(q, qWords, parses)
+  }
+  
+  def getArgs(pq: ParsedQuestion): List[AbstractedArg] = pq.parses.map(getArg)
+  
+  def abstractArg(pq: ParsedQuestion, arg: List[QWord]): List[QToken] = {
+    val qw = pq.qWords
+    val i = qw.indexOfSlice(arg)
+    if (i >= 0) {
+      qw.slice(0, i) ++ List(ArgVar) ++ qw.slice(i+arg.size, qw.size)
+    } else {
+      pq.qWords
+    }
+  }
+  
+  def isAbstracted(aq: List[QToken]) = aq.count(t => t == ArgVar) == 1
+  
+  def generateTemplates(c: QACluster): List[OutputRecord] = {
+    try {
+      val qs = c.questions.map(parseQuestion) 
+      val records = for (q1 <- qs; q2 <- qs; arg <- getArgs(q1); abs = abstractArg(q2, arg.arg); if isAbstracted(abs)) yield OutputRecord(abs, arg.conjunct)
+      records.toList.distinct
+    } catch {
+      case e: Throwable => {
+        e.printStackTrace(System.err)
+        List()
+      }
+    }
+  }
+  
+}
+
