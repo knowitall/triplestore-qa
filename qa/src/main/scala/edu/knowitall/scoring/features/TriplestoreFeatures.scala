8d97bdfc8a168e45e09b097f4345c16411eb6f43
Rob Bart <rbart@cs.washington.edu>
2013-10-07 09:23:46 -0700

    Add StrSim stopwords to set for field similarity feature.

--- qa/src/main/scala/edu/knowitall/scoring/features/TriplestoreFeatures.scala
+++ qa/src/main/scala/edu/knowitall/scoring/features/TriplestoreFeatures.scala
@@ -8,53 +8,52 @@ import edu.knowitall.triplestore.SolrClient
 import edu.knowitall.triplestore.CachedTriplestoreClient
 
 object TriplestoreFeatures {
-  
+
   val baseClient = SolrClient("http://rv-n12.cs.washington.edu:10893/solr/triplestore", 500)
   val client = CachedTriplestoreClient(baseClient, 10000)
-  
+
   object QueryFrequency extends AnswerGroupFeature("Log of query literals in the triplestore") {
-    
+
     import AnswerGroupFeatures.AnswerContainsDeterminer.determiners
-    
+
     val junkTokens = Set("a", "an", "the", "or", "and", "&") ++ determiners
     val splitRegex = "\\s+".r
-    
+
     def apply(group: AnswerGroup) = {
       val queries = group.derivations.map(_.etuple.equery).distinct
       def conjunctCounts(eq: ExecQuery) = eq match {
         case q: ExecConjunctiveQuery => {
           val literalFields = q.conjuncts.flatMap(_.literalFields)
           val counts = literalFields.map { case (field, value) =>
-            client.count(value.toConjunct(field))  
+            client.count(value.toConjunct(field))
           }
           counts
         }
         case _ => throw new RuntimeException("unknown query type.")
       }
-      
+
       val counts = queries flatMap conjunctCounts
-      
+
       val avg = counts.sum.toDouble / counts.size.toDouble
-      
+
       math.log(avg + 1)
     }
   }
 
   object AnswerFrequency extends AnswerGroupFeature("Log of Frequency of answer in the triplestore") {
-    
-    import AnswerGroupFeatures.AnswerContainsDeterminer.determiners
-    import QueryFrequency.splitRegex
-    val junkTokens = Set("a", "an", "the", "or", "and", "&") ++ determiners
-    
 
-    
+    import AnswerGroupFeatures.AnswerContainsDeterminer.determiners
+    import edu.knowitall.execution.StrSim.stops
+    import QueryFrequency.splitRegex
+    val junkTokens = Set("a", "an", "the", "or", "and", "&") ++ determiners ++ stops
+
     def apply(group: AnswerGroup) = {
-      
+
       val rawAnswer = group.alternates.head.head.toLowerCase
       val answerTokens = splitRegex.split(rawAnswer)
       val filteredTokens = answerTokens.filter(tok => !junkTokens.contains(tok)).toSeq
       val escapedTokens = filteredTokens map SolrClient.escape
-      val cleanAnswer = if (escapedTokens.nonEmpty) escapedTokens.mkString(" ") else "" 
+      val cleanAnswer = if (escapedTokens.nonEmpty) escapedTokens.mkString(" ") else ""
       val query = CountQuery(cleanAnswer)
       math.log(client.count(query).toDouble + 1)
     }
