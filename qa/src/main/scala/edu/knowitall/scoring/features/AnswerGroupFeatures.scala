c774599343aceda68cabe4b5fad35aa4b7ac63bb
Rob Bart <rbart@cs.washington.edu>
2013-10-07 09:24:29 -0700

    Update featureSet with MinConfidence and Postag match features.

--- qa/src/main/scala/edu/knowitall/scoring/features/AnswerGroupFeatures.scala
+++ qa/src/main/scala/edu/knowitall/scoring/features/AnswerGroupFeatures.scala
@@ -1,8 +1,10 @@
 package edu.knowitall.scoring.features
+
 import edu.knowitall.triplestore.CachedTriplestoreClient
 import edu.knowitall.triplestore.SolrClient
 import edu.knowitall.execution.AnswerGroup
 import edu.knowitall.execution.ExecQuery
+import edu.knowitall.execution.ExecTuple
 import edu.knowitall.execution.ConjunctiveQuery
 import edu.knowitall.execution.ExecConjunctiveQuery
 import edu.knowitall.execution.Search.TSQuery
@@ -23,13 +25,37 @@ import scala.language.implicitConversions
 abstract class AnswerGroupFeature(name: String) extends Feature[AnswerGroup, Double](name)
 
 object AnswerGroupFeatures extends FeatureSet[AnswerGroup, Double] {
-  
+
   implicit def boolToDouble(bool: Boolean) = if (bool) 1.0 else 0.0
-  
+
   def allTuples(group: AnswerGroup)  = group.derivations.map(_.etuple.tuple)
   def allQueries(group: AnswerGroup) = group.derivations.map(_.etuple.equery).distinct
-  
-  object MultipleNamespaces extends AnswerGroupFeature("Answer comes from multiple namespaces") {
+
+    /**
+   * Generic features that apply to any AnswerGroup
+   */
+  private val features: Seq[AnswerGroupFeature] = Seq(
+      MinConfidence,
+      NumberOfDerivations,
+      AnswerContainsNegation,
+      LiteralFieldsDifference,
+      MatchesPostagPatterns(Set("DT NN", "DT NNS", "DT JJ", "DT VGB"))
+    )
+
+//        /**
+//   * Generic features that apply to any AnswerGroup
+//   */
+//  private val features: Seq[AnswerGroupFeature] = Seq(
+//      NumberOfDerivations,
+//      AnswerContainsArticles,
+//      AnswerContainsDeterminer,
+//      AnswerContainsNegation,
+//      LiteralFieldsDifference,
+//      TriplestoreFeatures.AnswerFrequency,
+//      SystemFeatures.PostagRanking
+//    )
+
+  object QuestionQueryOverlap extends AnswerGroupFeature("Question / Query overlap") {
     def apply(group: AnswerGroup) = {
       def getNamespace(t: Tuple) = t.get("namespace").toString
       val tupleNamespaces = allTuples(group) map getNamespace
@@ -37,20 +63,61 @@ object AnswerGroupFeatures extends FeatureSet[AnswerGroup, Double] {
       distinctNamespaces.size.toDouble
     }
   }
-  
-  
-  
+
+  object MinConfidence extends AnswerGroupFeature("Minimum Confidence") {
+    val confRegex = ".*conf.*".r
+    val min = 0.05
+    def apply(group: AnswerGroup) = {
+      val tuples = group.derivations.map(_.etuple.tuple)
+      val allAttrs = tuples.flatMap(_.attrs.iterator.toSeq)
+      val confs = allAttrs.collect {
+        case (confRegex(), x: Float) => math.max(min, x.toDouble)
+      }
+      (min :: confs).min
+    }
+  }
+
+  // TODO: add to featureset after finishing new training data 9-26...
+  case class MatchesPostagPatterns(patterns: Set[String]) extends AnswerGroupFeature("Answer postags in " + patterns.toString) {
+    import edu.knowitall.execution.PostagAnswerGrouper.normalize
+    def apply(group: AnswerGroup) = {
+      val postags = normalize(group.alternates.head.head)
+      patterns.contains(postags)
+    }
+  }
+
   object NumberOfAlternates extends AnswerGroupFeature("Number of alternate forms of the answer") {
     def apply(group: AnswerGroup) = group.alternates.size.toDouble
   }
-  
+
   object NumberOfDerivations extends AnswerGroupFeature("Number of AnswerDerivations in the AnswerGroup") {
     def apply(group: AnswerGroup) = {
       val numDerivations = group.derivations.size.toDouble
       math.sqrt(numDerivations)
     }
   }
-  
+
+  object NumberUniqueTriples extends AnswerGroupFeature("Number of Unique Triples in the AnswerGroup") {
+
+    val requiredFieldNames = Seq("arg1", "rel", "arg2", "namespace")
+
+    def apply(group: AnswerGroup) = {
+      val etuples = group.derivations.map(_.etuple)
+      val tupleQueries = etuples.collect { case ExecTuple(tuple, ExecConjunctiveQuery(_, query)) => (tuple, query) }
+      val triples = tupleQueries.flatMap { case (tuple, query) =>
+        // get a map of conjunct name -> conjunct attrs
+        val conjName = query.conjuncts.map(_.name)
+        // extract the triple for each conjunct
+        conjName.map { prefix =>
+          val attrs = requiredFieldNames.map(f => prefix+"."+f)
+          attrs.flatMap(tuple.getString(_))
+        }
+      }
+      val distinctTriples = triples.distinct
+      triples.distinct.size
+    }
+  }
+
   object AnswerContainsArticles extends AnswerGroupFeature("Answer contains article tokens") {
     val articles = Set("the", "a", "an")
     def apply(group: AnswerGroup) = {
@@ -59,7 +126,7 @@ object AnswerGroupFeatures extends FeatureSet[AnswerGroup, Double] {
       articles.intersect(firstAnswerTokens).nonEmpty
     }
   }
-  
+
   object AnswerContainsDeterminer extends AnswerGroupFeature("Answer contains determiner") {
     val determiners = Set("these", "those", "that", "this", "some", "most", "all", "any", "both", "either", "each", "more", "less")
     def apply(group: AnswerGroup) = {
@@ -68,7 +135,7 @@ object AnswerGroupFeatures extends FeatureSet[AnswerGroup, Double] {
       determiners.intersect(firstAnswerTokens).nonEmpty
     }
   }
-  
+
   object AnswerContainsNegation extends AnswerGroupFeature("Answer contains a negation word") {
     val negationWords = Set("no", "none", "never", "neither", "nobody", "nor", "nothing", "nowhere", "n't", "cannot", "cant", "can't", "wont")
     def apply(group: AnswerGroup) = {
@@ -77,31 +144,15 @@ object AnswerGroupFeatures extends FeatureSet[AnswerGroup, Double] {
       negationWords.intersect(firstAnswerTokens).nonEmpty
     }
   }
-  
 
-  
-  /**
-   * Generic features that apply to any AnswerGroup
-   */
-  private val features: Seq[AnswerGroupFeature] = Seq(
-      NumberOfDerivations,
-      AnswerContainsArticles,
-      AnswerContainsDeterminer,
-      AnswerContainsNegation,
-      LiteralFieldsDifference,
-      TriplestoreFeatures.AnswerFrequency,
-      SystemFeatures.PostagRanking
-    )
-
-  
-  override val featureMap = 
+  override val featureMap =
     SortedMap.empty[String, Feature[AnswerGroup, Double]] ++ features.map(feature => (feature.name, feature))
 }
 
 object TupleFeatures {
-  
+
   import edu.knowitall.execution.Tuple
-  
+
   def isFromNamespace(ns: String)(tuple: Tuple) = tuple.get("namespace").exists {
     case s: String => s.contains(ns)
     case _ => false
