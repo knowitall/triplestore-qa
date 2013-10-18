d2ef6ffbdabefd2c3ae515f46ff2ee26ca721133
Anthony Fader <afader@gmail.com>
2013-10-17 18:46:38 -0700

    factored to have different paraphrase scorers

--- qa/src/main/scala/edu/knowitall/paraphrasing/template/PmiLmParaphraseScorer.scala
+++ qa/src/main/scala/edu/knowitall/paraphrasing/template/PmiLmParaphraseScorer.scala
@@ -3,11 +3,11 @@ package edu.knowitall.paraphrasing.template
 import edu.knowitall.tool.tokenize.ClearTokenizer
 import edu.knowitall.tool.stem.MorphaStemmer
 import edu.knowitall.lm.KenLmServer
+import edu.knowitall.paraphrasing.ScoredParaphraseDerivation
 
-
-class TemplateParaphraseScorer() {
+class PmiLmParaphraseScorer() extends ParaphraseScorer {
   val client = new KenLmServer()
-  def scoreAll(derivs: Iterable[TemplateParaphraseDerivation]) = {
+  override def scoreAll(derivs: Iterable[TemplateParaphraseDerivation]): Iterable[TemplateParaphraseDerivation] = {
     val dlist = derivs.toList
     val lmScores = client.query(dlist.map(_.paraphrase.question.mkString(" "))).map(_._2)
     for ((d, score) <- dlist.zip(lmScores)) yield d.copy(score = -score * d.templates.pmi) 
--- qa/src/main/scala/edu/knowitall/paraphrasing/template/TemplateParaphraser.scala
+++ qa/src/main/scala/edu/knowitall/paraphrasing/template/TemplateParaphraser.scala
@@ -6,10 +6,11 @@ import edu.knowitall.tool.tokenize.ClearTokenizer
 import org.slf4j.LoggerFactory
 import edu.knowitall.tool.stem.MorphaStemmer
 import edu.knowitall.paraphrasing.Paraphrase
+import com.typesafe.config.ConfigFactory
 
-case class TemplateParaphraser(scorer: TemplateParaphraseScorer, generator: TemplateParaphraseGenerator) extends Paraphraser {
+case class TemplateParaphraser(scorer: ParaphraseScorer, generator: TemplateParaphraseGenerator) extends Paraphraser {
   
-  def this() = this(new TemplateParaphraseScorer(), new SolrParaphraseGenerator())
+  def this() = this(TemplateParaphraser.defaultScorer, new SolrParaphraseGenerator())
   
   lazy val tagger = new StanfordPostagger()
   lazy val tokenizer = new ClearTokenizer()
@@ -25,11 +26,25 @@ case class TemplateParaphraser(scorer: TemplateParaphraseScorer, generator: Temp
   override def paraphrase(s: String) = {
     val stemmed = stemString(s)
     val paraphrases = generator.generate(stemmed)
-    val scored = scorer.scoreAll(paraphrases)
+    val scored = scorer.scoreAll(paraphrases).toList
     val grouped = scored.groupBy(sp => sp.paraphrase.question).values
     val maxed = grouped.map(g => g.maxBy(d => d.score)).toList
     val sorted = maxed.sortBy(d => -d.score)
     for (deriv <- sorted; target = deriv.paraphrase.question.mkString(" "))
       yield Paraphrase(s, target, deriv)
   }
+}
+
+case object TemplateParaphraser {
+  
+  val conf = ConfigFactory.load()
+  val scoringModel = conf.getString("paraphrase.scoringModel")
+  val defaultScorer = scoringModel match {
+    case "pmi" => new PmiParaphraseScorer()
+    case "lm" => new LmParaphraseScorer()
+    case "pmiLm" => new PmiLmParaphraseScorer()
+    case _ => throw new IllegalStateException(s"Could not load default scoring model '$scoringModel'")
+  }
+
+  
 }
