7422e826a30c494dd6a2704be70f5c16f74b3897
Anthony Fader <afader@gmail.com>
2013-10-26 15:21:25 -0700

    feature function code

---
+++ qa/src/main/scala/edu/knowitall/scoring/learning/FeatureFunction.scala
@@ -0,0 +1,17 @@
+package edu.knowitall.scoring.learning
+import scala.language.implicitConversions
+
+abstract class FeatureFunction[T] extends Function[T, SparseVector] {
+  override def apply(t: T): SparseVector
+  def sum(that: FeatureFunction[T]): FeatureFunction[T]
+  def +(that: FeatureFunction[T]) = this.sum(that)
+}
+
+object FeatureFunction {
+  def apply[T](f: T => SparseVector): FeatureFunction[T] = FeatureFunctionImpl(f)
+  implicit def functionToFeatureFunction[T](f: T => SparseVector): FeatureFunction[T] = FeatureFunction(f)
+  private case class FeatureFunctionImpl[T](f: Function[T, SparseVector]) extends FeatureFunction[T] {
+    override def apply(t: T) = f(t)
+    override def sum(that: FeatureFunction[T]): FeatureFunction[T] = FeatureFunctionImpl((t: T) => this(t) + that(t)) 
+  }
+}
