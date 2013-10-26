6ec8e6bb609ac2d6c9e4d083b57a7ecd0c8cc749
Anthony Fader <afader@gmail.com>
2013-10-25 20:31:05 -0700

    not needed

--- qa/src/main/scala/edu/knowitall/scoring/learning/LinearScorer.scala
+++
@@ -1,5 +0,0 @@
-package edu.knowitall.scoring.learning
-
-case class LinearScorer[T](features: Function[T, SparseVector], weights: SparseVector) {
-  def apply(t: T): Double = features(t) * weights 
-}
