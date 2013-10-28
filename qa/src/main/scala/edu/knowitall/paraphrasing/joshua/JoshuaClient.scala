a238e6c61e030dece6d7f28db4c2cb16e2da7e94
Anthony Fader <afader@gmail.com>
2013-10-28 13:38:39 -0700

    made joshua output a scoredderivation

--- qa/src/main/scala/edu/knowitall/paraphrasing/joshua/JoshuaClient.scala
+++ qa/src/main/scala/edu/knowitall/paraphrasing/joshua/JoshuaClient.scala
@@ -10,8 +10,9 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.io.OutputStreamWriter
+import edu.knowitall.paraphrasing.ScoredParaphraseDerivation
 
-case class JoshuaOutput(output: String, score: Double)
+case class JoshuaOutput(output: String, score: Double) extends ScoredParaphraseDerivation
 
 case object JoshuaOutput {
   val pattern = """^(.*)\t(.*)$""".r
