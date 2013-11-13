c31258a4847dc3354e00fcc423461360cc68abf8
Anthony Fader <afader@gmail.com>
2013-11-12 20:31:11 -0800

    new learning scheme

--- qa/src/main/scala/edu/knowitall/scoring/learning/QATrainer.scala
+++ qa/src/main/scala/edu/knowitall/scoring/learning/QATrainer.scala
@@ -7,23 +7,37 @@ import scala.io.Source
 import edu.knowitall.eval.FileOracle
 import edu.knowitall.apps.QASystem
 import edu.knowitall.apps.AnswerDerivation
+import java.io.File
+import java.io.PrintWriter
+import edu.knowitall.util.Counter
 
 object QATrainer extends App {
+
   val conf = ConfigFactory.load()
 
   val labelsPath = args(0)
-  val inputsPath = args(1)
-  val modelOutput = args(2)
+  val inputsPath = args(1)  
+  val dir = new File(args(2))
+  if (dir.exists() && !dir.isDirectory()) 
+    throw new IllegalStateException(s"$dir exists but is not a directory")
+  if (!dir.exists()) dir.mkdirs()
+  val modelOutput = new File(dir, "model.txt")
+  
+  val configOutput = new PrintWriter(new File(dir, "config.txt"))
+  configOutput.write(conf.root().render)
+  configOutput.close()
+  
   val numIters = conf.getInt("perceptron.numIters")
   
-  val oracle = new FileOracle(labelsPath)
+  val oracle = new MemoryInteractiveOracle(labelsPath) //new LabeledDataOracle(labelsPath)
   val inputs = Source.fromFile(inputsPath, "UTF8").getLines.map(Oracle.normalize).toList
-  val training = oracle.toTrainingSet.filter(pair => inputs.contains(Oracle.normalize(pair._1)))
   
   val generator = QASystem.getInstance().get
   val model = QAModel(generator)
-  val learner = new HiddenVariblePerceptron[String, AnswerDerivation, Set[String]](model)
-  val learnedModel = learner.trainAverage(training, numIters)
-  SparseVector.toFile(learnedModel.weights, modelOutput)
+  
+  val learner = new NewPerceptron(model, oracle)
+  val learnedModel = learner.learn(inputs)
+  
+  SparseVector.toFile(model.weights, modelOutput.toString())
 
 }
