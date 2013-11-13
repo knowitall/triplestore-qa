21e51e50ab937473d8f68855654c9a0e9b1511c8
Anthony Fader <afader@gmail.com>
2013-11-12 20:30:28 -0800

    refactored api to be compatible w new perceptron

--- qa/src/main/scala/edu/knowitall/scoring/learning/HiddenVariableModel.scala
+++ qa/src/main/scala/edu/knowitall/scoring/learning/HiddenVariableModel.scala
@@ -1,13 +1,10 @@
 package edu.knowitall.scoring.learning
 
-trait HiddenVariableModel[Input, Hidden, Output] {
-  type Model = HiddenVariableModel[Input, Hidden, Output]
-  def predict(input: Input): Option[Hidden]
-  def predictConstrained(input: Input, output: Output): Option[Hidden]
-  def project(hidden: Hidden): Output
-  def score(input: Input, hidden: Hidden): Double
-  def update(input: Input, predicted: Hidden, expected: Hidden): Model
-  def isCorrect(input: Input, predicted: Output, expected: Output): Boolean
+trait HiddenVariableModel[Input, Output] {
+  type Model = HiddenVariableModel[Input, Output]
+  def predict(input: Input): Option[Output]
+  def candidatePredictions(input: Input): Seq[Output]
+  def update(input: Input, predicted: Output, expected: Output): Unit
   def weights: SparseVector
   def sum(that: Model): Model
   def scale(c: Double): Model
