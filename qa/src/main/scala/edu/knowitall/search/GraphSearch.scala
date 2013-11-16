f4079bcf23176faf22d828cdfae7385e919e89a0
Anthony Fader <afader@gmail.com>
2013-11-15 17:50:12 -0800

    removed

--- qa/src/main/scala/edu/knowitall/search/GraphSearch.scala
+++
@@ -1,8 +0,0 @@
-package edu.knowitall.search
-
-import scala.collection.mutable.PriorityQueue
-import scala.collection.mutable.{Set => MutableSet}
-import scala.annotation.tailrec
-
-abstract class GraphSearch[State, Action] {
-}
