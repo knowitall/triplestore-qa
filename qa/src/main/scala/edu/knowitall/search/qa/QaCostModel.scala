2170eef5d7cc0c34f9d561a2b516d7fd0cc6ec4f
Anthony Fader <afader@gmail.com>
2013-11-19 23:40:02 -0800

    cost function for qa

---
+++ qa/src/main/scala/edu/knowitall/search/qa/QaCostModel.scala
@@ -0,0 +1,18 @@
+package edu.knowitall.search.qa
+
+import edu.knowitall.scoring.learning.SparseVector
+
+
+class QaCostModel(
+    features: Function[QaStep, SparseVector] = QaCostModel.defaultFeatures,
+    weights: SparseVector = QaCostModel.defaultWeights) extends Function[QaStep, Double] {
+  // multiply by -1 since search algos find minimum path
+  override def apply(step: QaStep) = -1.0 * (features(step) * weights)
+}
+
+object QaCostModel {
+  
+  val defaultFeatures = QaFeatures
+  val defaultWeights = SparseVector.zero
+  
+}
