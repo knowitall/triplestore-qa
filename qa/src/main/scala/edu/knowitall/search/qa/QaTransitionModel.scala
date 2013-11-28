65e34f7f16e04c85625bf4f3ab10ab3566042440
Anthony Fader <afader@gmail.com>
2013-11-27 20:39:42 -0800

    added relsyn transition

--- qa/src/main/scala/edu/knowitall/search/qa/QaTransitionModel.scala
+++ qa/src/main/scala/edu/knowitall/search/qa/QaTransitionModel.scala
@@ -9,6 +9,7 @@ import edu.knowitall.parsing.regex.RegexQuestionParser
 import edu.knowitall.triplestore.SolrClient
 import edu.knowitall.triplestore.CachedTriplestoreClient
 import edu.knowitall.search.Transition
+import edu.knowitall.relsyn.RelSynClient
 
 class QaTransitionModel extends Transition[QaState, QaAction] {
   
@@ -24,6 +25,7 @@ class QaTransitionModel extends Transition[QaState, QaAction] {
   lazy val templateClient = new ParaphraseTemplateClient
   lazy val baseTriplestoreClient = new SolrClient()
   lazy val triplestoreClient = CachedTriplestoreClient(baseTriplestoreClient)
+  lazy val relSynClient = RelSynClient()
   
   // Individual transition functions
   lazy val absArgTransition = new AbstractArgTransition(tagger = tagger,
@@ -31,10 +33,12 @@ class QaTransitionModel extends Transition[QaState, QaAction] {
   lazy val templateTransition = new TemplateTransition
   lazy val parseTransition = new RegexParseTransition(parser)
   lazy val executeTransition = new ExecutionTransition(triplestoreClient)
+  lazy val relSynTransition = new RelSynTransition(relSynClient)
   
   val model = absArgTransition + 
   			  templateTransition +
   			  parseTransition +
+  			  relSynTransition +
   			  executeTransition
   			  
   override def apply(s: QaState) = model(s)
