760165d8f5158dbabed2d90639232dcdf70b14dc
Anthony Fader <afader@gmail.com>
2013-12-02 17:09:00 -0800

    removed interval, fixed cats bug

--- qa/src/main/scala/edu/knowitall/parsing/cg/CKY.scala
+++ qa/src/main/scala/edu/knowitall/parsing/cg/CKY.scala
@@ -5,6 +5,7 @@ import edu.knowitall.repr.sentence.Lemmatized
 import edu.knowitall.repr.sentence.Chunked
 import edu.knowitall.collection.immutable.Interval
 import scala.collection.mutable.{Map => MutableMap}
+import edu.knowitall.util.MathUtils
 
 trait Node {
   def span: Interval
@@ -29,49 +30,34 @@ case class CKY[T](input: T, size: Int,
 				  terminalRules: IndexedSeq[TerminalRule[T]],
 				  combinators: IndexedSeq[Combinator]) {
   
-  val cats = MutableMap.empty[Interval, Category]
+  val cats = MutableMap.empty[Interval, Set[Category]]
   val nodes = MutableMap.empty[CatSpan, Node]
   
-  private def intervals(length: Int) = for {
-    i <- 0 until (size - length + 1)
-  } yield Interval.open(i, i + length)
-    
-  private def allIntervals = for {
-    length <- 1 to size
-    interval <- intervals(length)
-  } yield interval
-  
-  private def splits(interval: Interval) = for {
-    k <- (interval.head + 1) until interval.end
-    i = interval.head
-    j = interval.end
-  } yield (Interval.open(i, k), Interval.open(k, j))
-  
   private def applyTerminalRules = for {
-    interval <- allIntervals
+    interval <- MathUtils.allIntervals(size)
     rule <- terminalRules
     category <- rule(interval, input)
     catspan = CatSpan(category, interval)
     terminal = Terminal(catspan, rule)
   } yield {
-    cats += (interval -> category)
+    cats += (interval -> (cats.getOrElse(interval, Set.empty) + category))
     nodes += (catspan -> terminal)
   }
   
   private def applyCombinators(length: Int) = for {
-    interval <- intervals(length)
-    (left, right) <- splits(interval)
-    lcat <- cats.get(left)
-    rcat <- cats.get(right)
+    interval <- MathUtils.intervals(length, size)
+    (left, right) <- MathUtils.splits(interval)
+    lcat <- cats.getOrElse(left, Set.empty)
+    rcat <- cats.getOrElse(right, Set.empty)
     combinator <- combinators
     cat <- combinator(lcat, rcat)
     node = NonTerminal(CatSpan(cat, interval), CatSpan(lcat, left), CatSpan(rcat, right), combinator)
   } {
-    cats += (interval -> cat)
+    cats += (interval -> (cats.getOrElse(interval, Set.empty) + cat))
     nodes += (CatSpan(cat, interval) -> node)
   }
   
-  private def parse = {
+  def parse = {
     applyTerminalRules
     for (length <- 2 to size) applyCombinators(length)
   }
