7cfe16f7529433a26b281662de1b72d2ec754460
Anthony Fader <afader@gmail.com>
2013-12-09 16:16:33 -0800

    added lm retry

--- qa/src/main/scala/edu/knowitall/lm/LanguageModel.scala
+++ qa/src/main/scala/edu/knowitall/lm/LanguageModel.scala
@@ -8,6 +8,7 @@ import scala.io.Source
 import scalaj.http.HttpOptions
 import com.typesafe.config.ConfigFactory
 import edu.knowitall.util.MathUtils
+import org.slf4j.LoggerFactory
 
 trait LanguageModel {
   /**
@@ -23,11 +24,29 @@ trait LanguageModel {
 
 case class KenLmServer(url: String, timeOut: Int, scale: Boolean = KenLmServer.scale) extends LanguageModel {
   def this() = this(KenLmServer.defaultUrl, KenLmServer.defaultTimeout)
+  val logger = LoggerFactory.getLogger(this.getClass)
   val root = s"${url}/score"
-  override def query(s: String): Double = {
-    scaleValue(Http(root).params("q" -> s).asString.toDouble)
+  val retries = KenLmServer.retries
+  
+  override def query(s: String): Double = query(s, 1)
+  
+  private def query(s: String, attempt: Int = 1): Double = {
+    if (attempt > retries) {
+      throw new IllegalStateException("Unable to query KenLM for '$s'")
+    } else {
+      try {
+        logger.debug(s"Querying for one string (attempt $attempt/$retries): $s")
+        scaleValue(Http(root).params("q" -> s).asString.toDouble)
+      } catch {
+        case e: Throwable => {
+          query(s, attempt + 1)
+        }
+      }
+    }
   }
+
   def queryBatch(s: Iterable[String]) = {
+    logger.debug(s"Querying for ${s.size} strings")
     val lst = s.toList
     val joined = lst.mkString("|")
     val lines = Http.post(root).
@@ -50,6 +69,7 @@ case object KenLmServer {
   val conf = ConfigFactory.load()
   val defaultUrl = conf.getString("lm.url")
   val defaultTimeout = conf.getInt("lm.timeout")
+  val retries = conf.getInt("lm.retries")
   val batchSize = conf.getInt("lm.batchSize");
   val minValue = conf.getDouble("lm.minValue")
   val maxValue = conf.getDouble("lm.maxValue")
