8f15b5725a4e3dd9d3ecec8d87f2a4a109bfc1ef
Anthony Fader <afader@gmail.com>
2013-12-09 18:31:06 -0800

    nlptools in one place

---
+++ qa/src/main/scala/edu/knowitall/util/NlpTools.scala
@@ -0,0 +1,26 @@
+package edu.knowitall.util
+
+import edu.knowitall.tool.postag.StanfordPostagger
+import edu.knowitall.tool.stem.MorphaStemmer
+import edu.knowitall.tool.chunk.OpenNlpChunker
+import edu.knowitall.repr.sentence.Lemmatized
+import edu.knowitall.repr.sentence.Chunked
+import edu.knowitall.repr.sentence.Sentence
+import edu.knowitall.repr.sentence.Lemmatizer
+import edu.knowitall.repr.sentence.Chunker
+import edu.knowitall.tool.tokenize.ClearTokenizer
+
+object NlpTools {
+  lazy val tagger = new StanfordPostagger
+  lazy val stemmer = new MorphaStemmer
+  lazy val tokenizer = new ClearTokenizer
+  lazy val chunker = new OpenNlpChunker
+  lazy val dummyChunker = DummyChunker(tagger)
+  
+  def process(sentence: String): Sentence with Chunked with Lemmatized = {
+    new Sentence(sentence) with Chunker with Lemmatizer {
+      val chunker = NlpTools.dummyChunker
+      val lemmatizer = NlpTools.stemmer
+    }
+  }
+}
