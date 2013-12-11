ac70fd48cb84563c382fc1f1ded0dccd033e2d20
Anthony Fader <afader@gmail.com>
2013-12-10 22:57:34 -0800

    turned into class

--- qa/src/main/scala/edu/knowitall/learning/QATrainer.scala
+++ qa/src/main/scala/edu/knowitall/learning/QATrainer.scala
@@ -11,6 +11,33 @@ import java.io.File
 import java.io.PrintWriter
 import edu.knowitall.util.Counter
 import edu.knowitall.model.QaModel
+import edu.knowitall.model.Derivation
+
+class QaTrainer(model: QaModel, oracle: CorrectnessModel[String, Derivation]) extends HiddenVariableModel[String, Derivation] {
+  
+  private var avgWeights = model.costModel.weights
+  private var iter = 1.0
+  private val perceptron = new Perceptron(this, oracle)
+  
+  override def predict(question: String) = model.predict(question)
+  
+  override def candidatePredictions(question: String) = model.candidatePredictions(question)
+  
+  override def update(question: String, predicted: Derivation, expected: Derivation) = {
+    model.update(question, predicted, expected)
+    avgWeights = avgWeights + (expected.features - predicted.features) * iter
+  }
+  
+  def learnIter(question: String) = {
+    perceptron.learnIter(question)
+    iter += 1
+  }
+  
+  def learn(inputs: Traversable[String]) = inputs foreach learnIter
+  
+  def averagedWeights = model.costModel.weights - (avgWeights / iter)
+  
+}
 
 object QaTrainer extends App {
 
@@ -35,15 +62,16 @@ object QaTrainer extends App {
   
   val model = QaModel()
   
-  val learner = new Perceptron(model, oracle)
+  val trainer = new QaTrainer(model, oracle)
   println("Learning...")
   for (i <- 1 to numIters) {
-    learner.learn(inputs)
+    trainer.learn(inputs)
     val file = new File(dir, s"model.$i.txt")
     SparseVector.toFile(model.costModel.weights, file.toString())
   }
   println("Done learning")
   
   SparseVector.toFile(model.costModel.weights, modelOutput.toString())
+  SparseVector.toFile(trainer.averagedWeights, (new File(dir, s"model.avg.txt")).toString())
 
 }
