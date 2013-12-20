a4c95512819054478611dd460ee9b340eb5e80fb
Anthony Fader <afader@gmail.com>
2013-12-20 15:55:26 -0800

    updated repl to use new model

--- qa/src/main/scala/edu/knowitall/apps/SimpleRepl.scala
+++ qa/src/main/scala/edu/knowitall/apps/SimpleRepl.scala
@@ -1,39 +1,26 @@
 package edu.knowitall.apps
 
 import jline.console.ConsoleReader
-import edu.knowitall.triplestore.SolrClient
-import edu.knowitall.execution.Joiner
-import edu.knowitall.execution.Tuple
-import edu.knowitall.execution.TConjunct
-import edu.knowitall.execution.Search.ProjectTriples
-import edu.knowitall.execution.Tabulator.tuplesToTable
+import edu.knowitall.model.QaModel
+import edu.knowitall.model.Derivation
 
-case class SimpleRepl(url: String = "http://rv-n12:8983/solr/triplestore", 
-    hits: Int = 500) {
-  
-  val client = SolrClient(url, hits)
-  val joiner = Joiner(client)
-  
-  def toTable(ts: List[Tuple], n: Integer): String = {
-    val colSuffs = List("arg1", "rel", "arg2", "namespace")
-    val cols = for (i <- Range(0, n); s <- colSuffs) yield s"r$i.$s"
-    tuplesToTable(cols.toList, ts)
+class SimpleRepl {
+  val model = QaModel()
+  def result(d: Derivation) = {
+    s"${d.answer}\n${d.score}\n$d\n${d.explainScore(model.costModel.weights)}\n"
   }
-  
-  def eval(input: String): String = {
-    val qs = TConjunct.fromStringMult(input)
-    val tuples = ProjectTriples(joiner.joinQueries(qs))
-    return toTable(tuples.toList, qs.size)
-  } 
-
+  def eval(input: String) = {
+    val candidates = model.candidatePredictions(input)
+    candidates map result mkString("\n")
+  }
 }
 
 object SimpleRepl extends App {
   
   override def main(args: Array[String]) = {
     import jline.console.ConsoleReader
-    val repl = SimpleRepl()
     val reader = new ConsoleReader()
+    val repl = new SimpleRepl
     while (true) {
 	  val line = reader.readLine("> ")
 	  val result = repl.eval(line)
@@ -41,12 +28,4 @@ object SimpleRepl extends App {
     }
   }
 
-}
-
-object CommandLine extends App {
-  override def main(args: Array[String]) = {
-    val repl = SimpleRepl()
-    val input = args.mkString(" ")
-    println(repl.eval(input))
-  }
 }
