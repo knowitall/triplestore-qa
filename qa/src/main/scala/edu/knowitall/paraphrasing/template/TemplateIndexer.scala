2b101071593a3c727e9f9b8c608391df84647573
Anthony Fader <afader@gmail.com>
2014-01-08 13:28:36 -0800

    updated to reflect new templates

--- qa/src/main/scala/edu/knowitall/paraphrasing/template/TemplateIndexer.scala
+++ qa/src/main/scala/edu/knowitall/paraphrasing/template/TemplateIndexer.scala
@@ -25,9 +25,9 @@ case class ParaphraseTemplateClient(solrUrl: String, maxHits: Int, scale: Boolea
   server.setMaxRetries(1)
   val searchField = "template1_exact"
   def paraphrases(s: String, limit: Int = maxHits) = {
-    val query = new SolrQuery(s"""${searchField}:"${s}"""")
+    val query = new SolrQuery(s"""${searchField}:"${s} AND typ_exact:anything"""")
     query.setRows(maxHits)
-    query.addSort(new SortClause("pmi", SolrQuery.ORDER.desc))
+    query.addSort(new SortClause("typPmi", SolrQuery.ORDER.desc))
     query.setParam("shards.tolerant", true)
     logger.debug(s"Sending query: ${query.toString()}")
     val resp = server.query(query)
@@ -51,30 +51,29 @@ case object ParaphraseTemplateClient {
   val defaultTimeout = conf.getInt("paraphrase.template.timeout")
 }
 
-case class TemplatePair(template1: String, template2: String, pmi: Double, count1: Double, count2: Double, jointCount: Double) extends QaAction {
-  def this(t1: String, t2: String, j: Double, c1: Double, c2: Double) = this(t1, t2, TemplatePair.pmi(j, c1, c2), c1, c2, j)
-  def this(t1: String, t2: String, j: String, c1: String, c2: String) = this(t1, t2, j.toDouble, c1.toDouble, c2.toDouble)
-}
+case class TemplatePair(template1: String, template2: String, typ: String, count1: Double, count2: Double, count12: Double, typCount12: Double, typPmi: Double, pmi: Double) extends QaAction
 
 case object TemplatePair {
-  
-  def pmi(j: Double, m1: Double, m2: Double): Double = Math.log(j) - Math.log(m1) - Math.log(m2) 
-  
+    
   def fromString(s: String): Option[TemplatePair] = {
-    s.split("\t", 5) match {
-      case Array(t1, t2, js, ms1, ms2) => Some(new TemplatePair(t1, t2, js, ms1, ms2))
+    s.split("\t", 9) match {
+      case Array(t1, t2, typ, count1, count2, typCount12, count12, typPmi, pmi) =>
+         Some(TemplatePair(t1, t2, typ, count1.toDouble, count2.toDouble, typCount12.toDouble, count12.toDouble, typPmi.toDouble, pmi.toDouble))
       case _ => None
     }
   }
   def fromDocument(doc: SolrDocument): Option[TemplatePair] = {
     val t1obj: Any = doc.getFieldValue("template1")
     val t2obj: Any = doc.getFieldValue("template2")
-    val sobj: Any = doc.getFieldValue("pmi")
-    val count1obj: Any = doc.getFieldValue("marg_count1")
-    val count2obj: Any = doc.getFieldValue("marg_count2")
-    val jointobj: Any = doc.getFieldValue("joint_count")
-    (t1obj, t2obj, sobj, count1obj, count2obj, jointobj) match {
-      case (t1: String, t2: String, s: Float, c1: Float, c2: Float, j: Float) => Some(TemplatePair(t1, t2, s, c1, c2, j))
+    val typobj: Any = doc.getFieldValue("typ")
+    val count1obj: Any = doc.getFieldValue("count1")
+    val count2obj: Any = doc.getFieldValue("count2")
+    val typCount12Obj: Any = doc.getFieldValue("typCount12")
+    val count12Obj: Any = doc.getFieldValue("count12")
+    val typPmiObj: Any = doc.getFieldValue("typPmi")
+    val pmiObj: Any = doc.getFieldValue("pmi")
+    (t1obj, t2obj, typobj, count1obj, count2obj, typCount12Obj, count12Obj, typPmiObj, pmiObj) match {
+      case (t1: String, t2: String, typ: String, count1: Float, count2: Float, typCount12: Float, count12: Float, typPmi: Float, pmi: Float) => Some(TemplatePair(t1, t2, typ, count1, count2, typCount12, count12, typPmi, pmi))
       case _ => None
     }
   }
@@ -86,13 +85,16 @@ class TemplateIndexer(server: SolrServer) {
   
   def pairToDoc(pair: TemplatePair): SolrInputDocument = {
     val doc = new SolrInputDocument
-    doc.addField("template1", pair.template1, pair.pmi.toFloat)
-    doc.addField("template2", pair.template2, pair.pmi.toFloat)
+    doc.addField("template1", pair.template1)
+    doc.addField("template2", pair.template2)
+    doc.addField("typ", pair.typ)
+    doc.addField("count1", pair.count1)
+    doc.addField("count2", pair.count2)
+    doc.addField("typCount12", pair.typCount12)
+    doc.addField("count12", pair.count12)
+    doc.addField("typPmi", pair.typPmi)
     doc.addField("pmi", pair.pmi)
-    doc.addField("id", s"${pair.template1}|${pair.template2}")
-    doc.addField("marg_count1", s"${pair.count1}")
-    doc.addField("marg_count2", s"${pair.count2}")
-    doc.addField("joint_count", s"${pair.jointCount}")
+    doc.addField("id", (pair.template1 + pair.template2).hashCode.toString)
     doc
   }
   
