30057be11284368cba4c4d7aba6547381f34af3b
Anthony Fader <afader@gmail.com>
2014-01-20 21:11:44 -0800

    updated logs

--- qa/src/main/scala/edu/knowitall/learning/QATrainer.scala
+++ qa/src/main/scala/edu/knowitall/learning/QATrainer.scala
@@ -33,6 +33,7 @@ class QaTrainer(model: QaModel, oracle: CorrectnessModel[String, Derivation]) ex
     model.update(question, predicted, expected)
     numUpdates += 1
     avgWeights = avgWeights + (expected.features - predicted.features) * iter
+    logger.info(s"Updated weights:\n${avgWeights.toTable}")
   }
   
   def learnIter(question: String) = {
