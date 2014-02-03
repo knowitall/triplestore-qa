42c590cd35bdb3eadb72eed501b764a81f47d53a
Anthony Fader <afader@gmail.com>
2014-02-03 10:25:33 -0800

    multiple outputs

--- qa/src/main/scala/edu/knowitall/eval/qa/QASystemLabelGenerator.scala
+++ qa/src/main/scala/edu/knowitall/eval/qa/QASystemLabelGenerator.scala
@@ -5,11 +5,16 @@ import edu.knowitall.eval.SystemOutput
 import edu.knowitall.eval.FileOracle
 import edu.knowitall.eval.LabelGenerator
 
-class QASystemLabelGenerator(oracle: Oracle, output: QASystemOutput) extends LabelGenerator(oracle, output) {
+class QASystemLabelGenerator(oracle: Oracle, outputs: List[QASystemOutput]) extends LabelGenerator(oracle, outputs) {
+  
+  def recordFor(i: String, o: String) = {
+    val all = outputs.flatMap(_.qaRecordsFor(i, o)).distinct
+    if (all.isEmpty) None else Some(all(0))
+  }
   
   override def printLabel(in: String, out: String) = {
     println(s"LABEL\t0\t$in\t$out\n")
-    for (r <- output.qaRecordsFor(in, out).take(1); d = r.derivation) {
+    for (r <- recordFor(in, out).take(1); d = r.derivation) {
       val line = d.replaceAll(" => ", "\n=>\n")
       println(line)
     }
@@ -22,10 +27,10 @@ class QASystemLabelGenerator(oracle: Oracle, output: QASystemOutput) extends Lab
 object QASystemLabelGenerator extends App {
   val mode = args(0)
   val labelsPath = args(1)
-  val outputPath = args(2)
+  val outputPaths = args.slice(2, args.size).toList
   val oracle = new FileOracle(labelsPath)
-  val output = QASystemOutput.fromPath(outputPath)
-  val labeler = new QASystemLabelGenerator(oracle, output)
+  val outputs = outputPaths map QASystemOutput.fromPath
+  val labeler = new QASystemLabelGenerator(oracle, outputs)
   mode match {
     case "top" => labeler.generateTopLabelFile
     case "all" => labeler.generateLabelFile
