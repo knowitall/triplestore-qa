00035274537a5256d48f9dbf282294118e9119ea
Anthony Fader <afader@gmail.com>
2014-02-12 18:40:09 -0800

    changed around

--- qa/src/main/scripts/util/ablation_plot.py
+++ qa/src/main/scripts/util/ablation_plot.py
@@ -6,35 +6,29 @@ from numpy import std
 from numpy import mean
 import matplotlib.pyplot as plt
 
-output_file = 'ablation.pdf'
+output_file = 'ablation-kb.pdf'
 title = 'F1 when components are removed (relative to full system)'
-good = '#6CA0DC'
+good = 'gray'
 bad = 'red'
 maxval = 70
 width_in = 7
-height_in = 2.5
+height_in = 1.75
 
 data_dir = 'eval/qa/output/final'
 cols = ['webquestions', 'trec', 'wikianswers']
 rows = [
-    'title1',
+    'full',
     'noopenie', 
     'nofreebase', 
     'noprobase', 
     'nonell', 
-    '',
-    'title2',
-    'defaultweights',
-    'noparaphrases',
-    'maxconj1', 
-    'noqueryrewrites', 
-    '',
 ]
 row_names = {
-    'noopenie': 'Open IE',
-    'nofreebase': 'Freebase',
-    'noprobase': 'Probase',
-    'nonell': 'NELL',
+    'full': r'\textsc{System}',
+    'noopenie': 'Without Open IE',
+    'nofreebase': 'Without Freebase',
+    'noprobase': 'Without Probase',
+    'nonell': 'Without NELL',
     'noparaphrases': 'Paraphrases',
     'noqueryrewrites': 'Query Rewrites',
     'defaultweights': r'Weight Learning',
@@ -55,10 +49,8 @@ font = {'family' : 'serif',
         'size' : 9}
 matplotlib.rc('font', **font)
 matplotlib.rc('text', usetex=True)
-#matplotlib.rc(params)
 
 def read_data(row, col, default=(0.0, 0.0)):
-    #path = '%s/%s/system-%s/f1-top.txt' % (data_dir, col, row)
     path = '%s/%s/system-%s/bootstrap-f1.txt' % (data_dir, col, row)
     try:
         data = [float(x) for x in open(path)]
@@ -70,20 +62,14 @@ def read_data(row, col, default=(0.0, 0.0)):
         return default
 
 baseline_values = dict((col, read_data(baseline_row, col)[0]) for col in cols)
-def relative_change(value, col):
-    x = baseline_values[col]
-    #return 100 * (value - x) / x
-    return value - x
 
-rel_f1 = defaultdict(lambda: defaultdict(float))
+f1 = defaultdict(lambda: defaultdict(float))
 stds = defaultdict(lambda: defaultdict(float))
 for row in rows:
     for col in cols:
-        (raw, err) = read_data(row, col, default=(baseline_values[col], 0.0))
-        rel = relative_change(raw, col)
-        rel_f1[row][col] = rel
+        (raw, err) = read_data(row, col, default=(0, 0.0))
+        f1[row][col] = raw
         stds[row][col] = err
-        print row,col,rel,err
 
 fig = plt.figure(figsize=(width_in, height_in))
 
@@ -93,35 +79,36 @@ for (i, col) in enumerate(cols):
     cname = col_names[col]
     ax.set_title(cname, fontsize=9, y=1.0)
     pos = arange(len(rows))
-    val = [rel_f1[row][col] for row in rows]
+    val = [f1[row][col] for row in rows]
     errs = [stds[row][col] for row in rows]
-    colors = [good if rel_f1[row][col] < 0 else bad for row in rows]
-    ax.barh(pos, val, align='center', height=.75, color=colors, lw=0) 
+    colors = [good for row in rows]
+    ax.barh(pos, val, align='center', height=.65, color=colors, lw=0) 
     ax.errorbar(val, pos, xerr=errs, elinewidth=0.5, color='black', fmt=' ', capsize=0)
     ax.set_yticks(pos)
     ax.spines['top'].set_visible(False)
-    ax.spines['left'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_linewidth(0.5)
+    ax.spines['left'].set_linewidth(0.5)
+    ax.spines['left'].set_color('gray')
+    ax.spines['bottom'].set_color('gray')
     ax.xaxis.set_ticks_position('bottom')
-    ax.xaxis.set_tick_params(width=0.5)
+    ax.xaxis.set_tick_params(width=0.5, color='gray')
     ax.yaxis.set_tick_params(width=0)
     if i == 0:
-        #ax.yaxis.set_ticks(pos, tuple(row_names[r] for r in rows))
         ax.set_yticklabels([row_names[r] for r in rows])
     else:
         ax.set_yticklabels(['' for r in rows])
-    ax.axvline(0, ymin=0, ymax=.90, color='k', lw=0.5)
     maxval = max(abs(v) for v in val)
     maxvalerr = max(abs(v)+e for (v,e) in zip(val,errs))
-    ax.set_xlim((-maxvalerr * 1.1, maxvalerr * 1.1))
-    ax.set_xlabel('F1')
-    ax.xaxis.set_ticks([-maxval, 0, maxval])
+    ax.set_xlim((0, maxvalerr * 1.1))
+    ax.set_xlabel('Test F1', labelpad=-6)
+    ax.xaxis.set_ticks([0, baseline_values[col]])
     bval = '%0.2f' % baseline_values[col]
-    ax.xaxis.set_ticklabels(['%0.2f' % (baseline_values[col]-maxval), bval, '%0.2f' % (maxval + baseline_values[col])])
+    ax.xaxis.set_ticklabels([0, bval])
+    xticks = ax.xaxis.get_major_ticks()
+    xticks[0].set_visible(False)
+    ax.text(-.005, -1.65, '0')
 
 title_text = '{\\bf ' + title + '}'
-#fig.text(0.55, 0.05, title_text, horizontalalignment='center')
 fig.tight_layout()
-#fig.subplots_adjust(bottom=0.2)
 plt.savefig(output_file)
