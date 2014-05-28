9b86f0997e958bd5e97938b2987bfc230055cf8b
Anthony Fader <afader@gmail.com>
2014-05-28 16:32:41 -0700

    updates

--- qa/src/main/scala/edu/knowitall/triplestore/TriplestoreDumper.scala
+++ qa/src/main/scala/edu/knowitall/triplestore/TriplestoreDumper.scala
@@ -1,7 +1,7 @@
 package edu.knowitall.triplestore
 
 import org.apache.lucene.store.FSDirectory
-import org.apache.lucene.index.IndexReader
+import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.index.IndexableField
 import scala.collection.JavaConversions._
 import java.io.File
@@ -10,14 +10,16 @@ object TriplestoreDumper extends App {
   
   def fieldToPair(field: IndexableField) = (field.name, field.stringValue)
   
+   
+  
   override def main(args: Array[String]) {
-    if (args.size != 3) {
+    if (args.size != 2) {
       throw new IllegalArgumentException("Usage: class input-dir output-file")
     }
-    val input = args(1)
-    val output = args(2)
+    val input = args(0)
+    val output = args(1)	
     val index = FSDirectory.open(new File(input))
-    val reader = IndexReader.open(index)
+    val reader = DirectoryReader.open(index)
     for {
       i <- 0 until reader.numDocs;
       doc = reader.document(i)
