090829fa4bfc364fd4a2fbc6e49ad017bb4e196d
Anthony Fader <afader@gmail.com>
2014-05-29 12:55:29 -0700

    fixed a few bugs

--- qa/src/main/scripts/wolframalpha/run_qa.py
+++ qa/src/main/scripts/wolframalpha/run_qa.py
@@ -10,17 +10,20 @@ key = sys.argv[1]
 client = wolframalpha.Client(key)
 
 for line in sys.stdin:
-    question = line.strip()
-    print >>sys.stderr, 'Question: %s' % question
-    res = client.query(question)
-    pods = res.pods
-    if len(pods) >= 2 and pods[1].title == 'Result' and pods[1].text:
-        lines = pods[1].text.split('\n')
-        if len(lines) > 1: lines = [line.strip() for line in lines[1:] if line.strip()]
-        for (k, answer) in enumerate(lines):
-            row = [question, answer, -1 * k, pods[0].text]
-            print '\t'.join(str(x) for x in row)
-            print >>sys.stderr, 'Answer: %s' % answer
-    else:
-        print >>sys.stderr, 'Answer: None'
-    time.sleep(2)
+    try:
+        question = line.strip()
+        print >>sys.stderr, 'Question: %s' % question
+        res = client.query(question)
+        pods = res.pods
+        if len(pods) >= 2 and pods[1].title == 'Result' and pods[1].text:
+            lines = pods[1].text.split('\n')
+            if len(lines) > 1: lines = [line.strip() for line in lines[1:] if line.strip()]
+            for (k, answer) in enumerate(lines):
+                row = [question, answer, -1 * k, pods[0].text]
+                print '\t'.join(unicode(x) for x in row)
+                print >>sys.stderr, 'Answer: %s' % answer
+        else:
+            print >>sys.stderr, 'Answer: None'
+        time.sleep(2)
+    except Exception, e:
+        print >>sys.stderr, e
